4c0198fa333a5941210febe76fdcbe13debc988d
Joe <joe.murdoch@warwick.ac.uk>
2025-11-10 18:04:27 +0000

    feat(inverted-index): third implementation of inverse index

--- src/index/inverted_index.hpp
+++ src/index/inverted_index.hpp
@@ -4,118 +4,89 @@
 #include <unordered_map>
 #include <string>
 #include <algorithm>
+#include <set>
 
 template <typename Term, typename Doc>
 class InvertedIndex {
     private:
-        std::unordered_map<Term, std::vector<Doc>> index;
+        std::unordered_map<Term, std::set<Doc>> index;
 
-        std::vector<Doc> intersect(const std::vector<Doc>& a, const std::vector<Doc>& b);
+        /**
+        * Given two lists of DocIDs, returns their intersection.
+        */
+        static std::set<Doc> intersect(const std::set<Doc>& a, const std::set<Doc>& b);
 
-        std::vector<Doc> set_union(const std::vector<Doc>& a, const std::vector<Doc>& b);
-        
-        const std::vector<Doc>& get_postings(const Term& term) const;
+        /**
+         * Given two lists of DocIDs, returns their union.
+        */
+        static std::set<Doc> set_union(const std::set<Doc>& a, const std::set<Doc>& b);
+                
+        /**
+         * Returns a postings list for a given term.
+         */
+        const std::set<Doc>& get_postings(const Term& term) const;
 
+        /**
+         * Adds a posting 
+         */
         void add_posting(const Term& term, Doc doc_id);
         
     public:
         
+        /**
+         * Adds a document to the index.
+         */
         void add_document(Doc& doc_id, const std::vector<Term>& terms);
 
-        void finalise_index();
+        /**
+         * Searches for DocIDs that contain all given terms (AND query).
+         */
+        std::set<Doc> search_and(const std::vector<Term>& terms) const;
 
-        std::vector<Doc> search_and(const std::vector<Term>& terms) const;
-
-        std::vector<Doc> search_or(const std::vector<Term>& terms) const;
+        /**
+         * Searches for DocIDs that contain any given term (OR query).
+         */
+        std::set<Doc> search_or(const std::vector<Term>& terms) const;
 
 };
 
-
-/**
-* Given two lists of DocIDs, returns their intersection.
-*/
 template <typename Term, typename Doc>
-std::vector<Doc> InvertedIndex<Term, Doc>::intersect(const std::vector<Doc>& a, const std::vector<Doc>& b) {
-    std::vector<Doc> result;
-            
-    auto itr1 = a.begin();
-    auto itr2 = b.begin();
+std::set<Doc> InvertedIndex<Term, Doc>::intersect(const std::set<Doc>& a, const std::set<Doc>& b) {
+    std::set<Doc> result;
 
-    while (itr1 != a.end() && itr2 != b.end()) {
-        if (*itr1 == *itr2) {
-            result.push_back(*itr1);
-            itr1++;
-            itr2++;
-        }
-        else if (*itr1 < *itr2) {
-            itr1++;
-        }
-        else {
-            itr2++;
-        }
-    }
+    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::inserter(result, result.begin()));
 
     return result;
 }
 
-/**
- * Given two lists of DocIDs, returns their union.
-*/
 template <typename Term, typename Doc>
-std::vector<Doc> InvertedIndex<Term, Doc>::set_union(const std::vector<Doc>& a, const std::vector<Doc>& b) {
-    std::vector<Doc> result;
+std::set<Doc> InvertedIndex<Term, Doc>::set_union(const std::set<Doc>& a, const std::set<Doc>& b) {
+    std::set<Doc> result;
 
-    auto itr1 = a.begin();
-    auto itr2 = b.begin();
-            
-    while (itr1 != a.end() && itr2 != b.end()) {
-        if (*itr1 < *itr2) {
-            result.push_back(*itr1);
-            itr1++;
-        }
-        else if (*itr2 < *itr1) {
-            result.push_back(*itr2);
-            itr2++;
-        }
-        else {
-            result.push_back(*itr1);
-            itr1++;
-            itr2++;
-        }
-    }
-    
-    while (itr1 != a.end()) {
-        result.push_back(*itr1);
-        itr1++;
-    }
-    
-    while (itr2 != b.end()) {
-        result.push_back(*itr2);
-        itr2++;
-    }
+    std::set_union(a.begin(), a.end(), b.begin(), b.end(), std::inserter(result, result.begin()));
 
     return result;
 }
 
-/**
- * Returns a postings list for a given term.
- */
 template <typename Term, typename Doc>
-const std::vector<Doc>& InvertedIndex<Term, Doc>::get_postings(const Term& term) const {
-    return index.at(term);
+const std::set<Doc>& InvertedIndex<Term, Doc>::get_postings(const Term& term) const {
+    static const std::set<Doc> empty_set;
+    
+    auto itr = index.find(term);
+
+    if (itr != index.end()) {
+        return itr->second;
+    }
+    else {
+        return empty_set;
+    }
 }
 
-/**
- * Adds a posting 
- */
 template <typename Term, typename Doc>
 void InvertedIndex<Term, Doc>::add_posting(const Term& term, Doc doc_id) {
-    index[term].push_back(doc_id);
+    index[term].insert(doc_id);
 }
 
-/**
- * Adds a document to the index.
- */
 template <typename Term, typename Doc>
 void InvertedIndex<Term, Doc>::add_document(Doc& doc_id, const std::vector<Term>& terms) {
     for (const Term& term : terms) {
@@ -123,51 +94,32 @@ void InvertedIndex<Term, Doc>::add_document(Doc& doc_id, const std::vector<Term>
     }
 }
 
-
-/**
- * Sorts all postings lists and removes duplicates. Call after all documents are added.
- */
 template <typename Term, typename Doc>
-void InvertedIndex<Term, Doc>::finalise_index() {
-    for (auto& pair : index) {
-        std::vector<Doc>& postings = pair.second;
-        
-        std::sort(postings.begin(), postings.end());
-        
-        auto last = std::unique(postings.begin(), postings.end());
-        postings.erase(last, postings.end());
-    }
-}
-
-
-/**
- * Searches for DocIDs that contain all given terms (AND query).
- */
-template <typename Term, typename Doc>
-std::vector<Doc> InvertedIndex<Term, Doc>::search_and(const std::vector<Term>& terms) const {
+std::set<Doc> InvertedIndex<Term, Doc>::search_and(const std::vector<Term>& terms) const {
     if (terms.empty()) {
         return {};
     }
-    std::vector<Doc> result = get_postings(terms[0]);
-    for (int i = 1; i < terms.size(); ++i) {
-        std::vector<Doc>& postings_list = get_postings(terms[i]);
+    
+    std::set<Doc> result = get_postings(terms[0]);
         
+    for (int i = 1; i < terms.size(); ++i) {
+        const std::set<Doc>& postings_list = get_postings(terms[i]);
         result = intersect(result, postings_list);
     }
+
     return result;
 }
 
-/**
- * Searches for DocIDs that contain any given term (OR query).
- */
 template <typename Term, typename Doc>
-std::vector<Doc> InvertedIndex<Term, Doc>::search_or(const std::vector<Term>& terms) const {
+std::set<Doc> InvertedIndex<Term, Doc>::search_or(const std::vector<Term>& terms) const {
     if (terms.empty()) {
         return {};
     }
-    std::vector<Doc> result = get_postings(terms[0]);
+    
+    std::set<Doc> result = get_postings(terms[0]);
+
     for (int i = 1; i < terms.size(); ++i) {
-        std::vector<Doc>& postings_list = get_postings(terms[i]);
+        const std::set<Doc>& postings_list = get_postings(terms[i]);
         result = set_union(result, postings_list);
     }
     return result;
